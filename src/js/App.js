c614f98fe2b5473fdf8e73545848123a1102892f
kras174 <kras32a@gmail.com>
2020-05-28 18:18:49 +0500

    first commit

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Note: You can remove this file if you wanna use only components like modals etc
+import HelloWorld from '@/components/HelloWorld.jsx'
+
+// Wrapers + Router paths is gonna be here!
+const App = () => {
+  return (
+    // example compoentnt
+    <HelloWorld />
+  )
+};
+
+export default App;
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// Note: You can import modules and use render for components if you wanna use only components like modals etc
+const el = document.getElementById("root");
+
+ReactDOM.render(
+  <App />,
+  el
+);
